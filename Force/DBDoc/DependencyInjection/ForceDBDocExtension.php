7d8ab37ff7ffb95293d5fe11f6f4bcd97581120d
matthieu88160 <matthieu.vallance@cscfa.fr>
2018-12-05 15:11:09 +0100

    Update controller to return a response

--- Force/DBDoc/DependencyInjection/ForceDBDocExtension.php
+++ Force/DBDoc/DependencyInjection/ForceDBDocExtension.php
@@ -20,6 +20,6 @@ class ForceDBDocExtension extends Extension
         $loader->load('services.yaml');
 
         $definition = $container->getDefinition(DocumentorController::class);
-        $definition->addMethodCall('setContent', [$config['return_content']])
+        $definition->addMethodCall('setContent', [$config['return_content']]);
     }
 }
